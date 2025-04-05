a9561c133156a75593a37d2f2a7cd6db2fbac7f5
Jiaxuan Yu <166388707+WZHDXXT@users.noreply.github.com>
2025-04-05 11:02:56 +0200

    Add files via upload

---
+++ RS/A1/Code/GMF.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+class GMF(nn.Module):
+    def __init__(self, num_users, num_items, latent_dim):
+        super(GMF, self).__init__()
+
+        self.user_embedding = nn.Embedding(num_users, latent_dim)
+        self.item_embedding = nn.Embedding(num_items, latent_dim)
+
+        nn.init.kaiming_normal_(self.user_embedding.weight, nonlinearity='relu')
+        nn.init.kaiming_normal_(self.user_embedding.weight, nonlinearity='relu')
+
+        self.fc = nn.Linear(latent_dim, 1)
+
+    def forward(self, user_indices, item_indices):
+        device = next(self.parameters()).device
+
+        user_indices = user_indices.to(device)
+        item_indices = item_indices.to(device)
+        user_latent = self.user_embedding(user_indices).to(self.user_embedding.weight.device)
+        item_latent = self.item_embedding(item_indices).to(self.user_embedding.weight.device)
+        prediction = torch.mul(user_latent, item_latent).sum(dim=1, keepdim=True)
+        # prediction = torch.sigmoid(interaction)
+        return prediction
